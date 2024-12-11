6b54083511c8cef5563f393f1334cfb0593de650
pothirendirahul <pothirendi.r@northeastern.edu>
2024-12-11 00:04:51 -0500

    Crewai-newsletter

---
+++ crewgooglegemini/crew.py
@@ -0,0 +1,11 @@
+from crewai import Crew,Process
+from tasks import research_task,write_task
+from agents import news_researcher,news_writer
+
+## Forming the tech focused crew with some enhanced configuration
+crew=Crew(
+    agents=[news_researcher,news_writer],
+    tasks=[research_task,write_task],
+    process=Process.sequential,
+
+)
